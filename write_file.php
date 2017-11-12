2b37c42df8e1a037d8927bfb3d473f9345ed9825
John van Dijk <jvandijk.mail@gmail.com>
2017-11-11 23:44:31 -0500

    First commit.
    Can send the yes/ no data without moving off of the page that you are on.

---
+++ write_file.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+	
+<?php
+
+
+function getGUID(){
+	// From http://guid.us/GUID/PHP
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }else{
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = chr(123)// "{"
+            .substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12)
+            .chr(125);// "}"
+        return $uuid;
+    }
+}
+
+// TODO: You will need to set the directory to full access in order for it to be written to.
+
+// Create the file with a date-stamped and guid-stamped filename.
+$guid = getGUID();  												// This can be changed if in windows.
+//echo $guid;
+$timestamp = date("Y-m-d-h-i-sa");
+
+$my_file = 'feedback_forms/'.$timestamp.'-'.$guid;
+$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+
+$data = $_POST["response"].'\n'.$timestamp.'\n'.$_POST["send_url"];
+fwrite($handle, $data);
+echo ('That file saved');
+?>
+
+</body>
+
+</html>
